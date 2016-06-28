ade3eb617f87e14ee4c67ee48ddba1b9d71e4081
Abhi Khanal <khanal.abhi@gmail.com>
2016-06-27 22:07:40 -0400

    Added a few classes.

---
+++ rpgoop/DevilWizard.swift
@@ -0,0 +1,14 @@
+//
+//  DevilWizard.swift
+//  rpgoop
+//
+//  Created by Abhinash Khanal on 6/27/16.
+//  Copyright © 2016 KHANALCO. All rights reserved.
+//
+
+import Foundation
+
+class DevilWizard: Enemy {
+    
+    
+}
--- rpgoop/Kimara.swift
+++ rpgoop/Kimara.swift
@@ -10,6 +10,26 @@ import Foundation
 
 class Kimara: Enemy {
     
+    let IMMUNE_MAX = 15;
     
+    override var loot: [String] {
+        get {
+            return ["A lot of gold", "Fancy pipe", "Price's yellow guitar"];
+        }
+    }
     
+    override var type: String {
+        get {
+            return "Kimara";
+        }
+    }
+    
+    override func attemptAttack(ap: Int) -> Bool {
+        if(ap >= IMMUNE_MAX){
+            return super.attemptAttack(ap);
+        } else {
+            return false;
+        }
+    }
+
 }
