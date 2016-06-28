2076c7259c918e47e04e81939177639e3a4059e5
Abhi Khanal <khanal.abhi@gmail.com>
2016-06-27 22:53:24 -0400

    Added attack button.

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -14,10 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerHpLabel: UILabel!
     @IBOutlet weak var enemyHpLabel: UILabel!
     @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestButton: UIButton!
+    
+    var player: Player!
+    var enemy: Enemy!
+    
+    @IBAction func onChestTapped(sender: AnyObject) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        player = Player(name: "Jhonny Guitar", hp: 110, ap: 20);
+        playerHpLabel.text = "\(player.hp) HP";
+        
+    }
+    
+    func generateRandomEnemy(){
+        let rand = Int(arc4random_uniform(2));
+        switch rand {
+        case 0:
+            enemy = Kimara(hp: 150, ap: 15);
+        case 1:
+            enemy = DevilWizard(hp: 100, ap: 10);
+        default:
+            enemy = DevilWizard(hp: 100, ap: 10);
+        }
     }
 
     override func didReceiveMemoryWarning() {
